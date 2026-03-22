48079c16cce94fe9a74da8b08ba6072c701878eb
Tyra-MO <tyramoraa87@gmail.com>
2026-03-22 23:16:49 +0300

    Create javascript app.js

---
+++ javascript app.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter student name: ");
+let units = parseInt(prompt("Enter number of registered units: "));
+
+let status;
+if (units > 7) {
+    status = "Overload - Approval Required";
+} else {
+    status = "Registration Accepted";
+}
+
+console.log("\n--- Registration Summary ---");
+console.log("Name:", name);
+console.log("Units:", units);
+console.log("Status:", status);
